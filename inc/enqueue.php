8982cb110341ebb1ec5e98ec72b2040a321f427c
Brian Coords <bacoords@gmail.com>
2017-03-30 12:50:24 -0700

    add stickyfill script

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		wp_enqueue_style( 'google-fonts-open-sans', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700', array(), '1.0.0' );
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'select2Buttons', get_template_directory_uri() . '/js/jQuery.select2Buttons.js', array(jquery), '1.0.0', true  );
-		wp_enqueue_script( 'stickyfill', get_template_directory_uri() . '/js/stickyfill.min.js', array(), '1.0.0', true  );
+		wp_enqueue_script( 'stickyfill', get_template_directory_uri() . '/js/stickyfill.min.js', array(jquery), '1.0.0', true  );
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
